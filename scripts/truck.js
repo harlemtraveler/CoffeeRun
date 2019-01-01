b94ac62df37b9bafd07c61b8cafb3a73af9bd778
Tim <tjk315@gmail.com>
2019-01-01 18:14:45 -0500

    Add "deliverOrder()" & "printOrder()" functions to truck.js

--- scripts/truck.js
+++ scripts/truck.js
@@ -12,6 +12,20 @@
     this.db.add(order.emailAddress, order);
   };
 
+  Truck.prototype.deliverOrder = function () {
+    console.log('Delivering order for ' + customerId);
+    this.db.remove(customerId);
+  };
+
+  Truck.prototype.printOrders = function () {
+    const customerIdArray = Object.keys(this.db.getAll());
+
+    console.log('Truck # ' + this.truckId + ' has pending orders:');
+    customerIdArray.forEach(function (id) {
+      console.log(this.db.get(id));
+    }.bind(this));
+  };
+
   App.Truck = Truck;
   window.App = App;
 })(window);
